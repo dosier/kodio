94dc9621f3e16b08422befb2eaa4061873cc8315
dosier <stanvdbend@gmail.com>
2025-09-21 05:55:18 +0200

    Added `AudioDevice.encodeAsByteArray` and `ByteArray.decodeAsAudioDevice` helpers

--- kodio-core/src/commonMain/kotlin/space/kodio/core/io/AudioDeviceCodec.kt
+++ kodio-core/src/commonMain/kotlin/space/kodio/core/io/AudioDeviceCodec.kt
@@ -1,10 +1,23 @@
 package space.kodio.core.io
 
 import kotlinx.io.Buffer
+import kotlinx.io.readByteArray
 import space.kodio.core.AudioDevice
 
 private const val INPUT_AUDIO_DEVICE_FLAG = 1.toByte()
 private const val OUTPUT_AUDIO_DEVICE_FLAG = 2.toByte()
+
+fun AudioDevice.encodeAsByteArray(): ByteArray {
+    val buffer = Buffer()
+    buffer.writeAudioDevice(this)
+    return buffer.readByteArray()
+}
+
+fun ByteArray.decodeAsAudioDevice(): AudioDevice {
+    val buffer = Buffer()
+    buffer.write(this)
+    return buffer.readAudioDevice()
+}
 
 fun Buffer.writeAudioDevice(audioDevice: AudioDevice) {
     when(audioDevice) {
