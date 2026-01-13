cc36e973e96a04f130910f67c63bc850b240d3fe
dosier <stanvdbend@gmail.com>
2026-01-13 21:39:29 +0100

    feat: add Maven Central publishing for compose extensions

--- kodio-extensions/compose/build.gradle.kts
+++ kodio-extensions/compose/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.composeHotReload)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 group = "space.kodio.extensions"
@@ -97,5 +98,38 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "compose", version.toString())
+
+    pom {
+        name = "Kodio Compose"
+        description = "Compose extensions for Kodio audio library."
+        inceptionYear = "2025"
+        url = "https://github.com/dosier/kodio"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "dosier"
+                name = "Stan"
+                url = "https://github.com/dosier"
+            }
+        }
+        scm {
+            url = "https://github.com/dosier/kodio"
+            connection = "scm:git:git://github.com/dosier/kodio.git"
+            developerConnection = "scm:git:ssh://git@github.com/dosier/kodio.git"
+        }
     }
 }
